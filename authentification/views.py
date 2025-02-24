345111a278f41b12d8fd08c222bc6cca5c56ddb2
gemo <kim.gemo@gmail.com>
2025-02-24 11:37:36 +0100

    must be superuser to access users endpoints

--- authentification/views.py
+++ authentification/views.py
@@ -1,14 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
 
 from authentification.models import User
 from authentification.serializers import UserSerializer
 from authentification.serializers import UserUpdateSerializer
 
-
 class UserViewset(ModelViewSet):
 
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAdminUser,]
 
     def get_serializer_class(self):
         if self.action == "update" or self.action == "partial_update":
