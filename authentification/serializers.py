e9347e5610f3f6b83a0546f5e6f793d3310e4b06
gemo <kim.gemo@gmail.com>
2025-02-24 09:41:01 +0100

    remove password for updates on users

--- authentification/serializers.py
+++ authentification/serializers.py
@@ -5,11 +5,25 @@ from authentification.models import User
 
 
 class UserSerializer(ModelSerializer):
-    
+
     def validate_password(self, value: str) -> str:
         return make_password(value)
 
     class Meta:
         model = User
-        fields = ["id", "username", "password", "age", "can_be_contacted", "can_data_be_shared"]
-        extra_kwargs = {'password': {'write_only': True, 'min_length': 4}}
+        fields = [
+            "id",
+            "username",
+            "password",
+            "age",
+            "can_be_contacted",
+            "can_data_be_shared",
+        ]
+        extra_kwargs = {"password": {"write_only": True, "min_length": 4}}
+
+
+class UserUpdateSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "age", "can_be_contacted", "can_data_be_shared"]
--- authentification/views.py
+++ authentification/views.py
@@ -2,9 +2,16 @@ from rest_framework.viewsets import ModelViewSet
 
 from authentification.models import User
 from authentification.serializers import UserSerializer
+from authentification.serializers import UserUpdateSerializer
 
 
 class UserViewset(ModelViewSet):
 
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "update" or self.action == "partial_update":
+            return UserUpdateSerializer
+        else:
+            return super().get_serializer_class()
