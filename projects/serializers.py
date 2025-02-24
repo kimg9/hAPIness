f804c9bdf841ae6bb5398b97bdf9514dd5298dee
gemo <kim.gemo@gmail.com>
2025-02-24 11:25:18 +0100

    flake8

--- projects/serializers.py
+++ projects/serializers.py
@@ -7,19 +7,19 @@ from projects.models import Comment, Issue, Project
 class ProjectSerializer(ModelSerializer):
     class Meta:
         model = Project
-        fields = '__all__'
-        read_only_fields = ('author', 'created_time')
+        fields = "__all__"
+        read_only_fields = ("author", "created_time")
 
 
 class IssueSerializer(ModelSerializer):
     class Meta:
         model = Issue
-        fields = '__all__'
-        read_only_fields = ('author', 'created_time')
+        fields = "__all__"
+        read_only_fields = ("author", "created_time")
 
 
 class CommentSerializer(ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
-        read_only_fields = ('author', 'created_time')
+        fields = "__all__"
+        read_only_fields = ("author", "created_time")
